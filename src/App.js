e6b9e1fbfcdf11f5ad56e016c5fc23792c3ada41
shravan20 <mrshravankumarb@gmail.com>
2024-04-05 20:28:44 +0530

    feat: add announcement slider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Body from './components/templates/Body';
 import Navbar from './components/atoms/Navbar';
+import AnnouncementSlider from './components/atoms/AnnouncementSlider';
+
 const App = () => {
 
 
@@ -8,6 +10,12 @@ const App = () => {
     <>
       <Navbar />
       <Body />
+
+      <AnnouncementSlider announcement={{
+        title: "Stay Tuned! Product Under Development!  ",
+        description: "Checkout issues!",
+        hyperlink: "https://github.com/shravan20/fosstrak-india/issues"
+      }} />
     </>
 
   );
--- src/components/atoms/AnnouncementSlider.jsx
+++ src/components/atoms/AnnouncementSlider.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const AnnouncementSlider = ({ announcement }) => {
   return (
     <div className="fixed inset-x-0 bottom-0 p-4">
-      <div className="relative flex items-center justify-between gap-4 rounded-lg bg-indigo-600 px-4 py-3 text-white shadow-lg">
+      <div className="relative flex items-center justify-between gap-4 rounded-lg accent-content bg-indigo-600 px-4 py-3 text-white shadow-lg">
         <p className="text-sm font-medium">
           {announcement && announcement.title ? announcement.title : ""}
           <a
