073d7cd600c1224acf39c88ced50d0f1beb4aeaa
shravan20 <mrshravankumarb@gmail.com>
2024-04-05 19:14:25 +0530

    feat: Add navbar | events from data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import EventList from './components/molecules/EventList';
 import Navbar from './components/atoms/Navbar';
+import eventsData from './data/events-v1.json';
+
 const App = () => {
   const [events, setEvents] = useState([]);
 
@@ -12,17 +14,9 @@ const App = () => {
     });
   }
   useEffect(() => {
-    // Mock API call to fetch events (replace with your actual API call)
     const fetchEvents = async () => {
       try {
-        const response = [{
-          name: "Test Event", description: "Description Test event", startDate: toLocaleDateString(new Date()), endDate: toLocaleDateString(new Date()), location: "Mangalore", type: "Free"
-        }, {
-          name: "Test Event 2", description: "Description Test event 2", startDate: toLocaleDateString(new Date()), endDate: toLocaleDateString(new Date()), location: "Bangalore", type: "Paid"
-        }];
-        // const response = await fetch('https://api.example.com/events');
-        // const data = await response.json();
-        const data = response;
+        const data = eventsData;
         setEvents(data);
       } catch (error) {
         console.error('Error fetching events:', error);
--- src/components/atoms/Navbar.jsx
+++ src/components/atoms/Navbar.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div class="navbar bg-neutral text-neutral-content">
-      <button class="btn btn-ghost text-xl">FOSSTrak</button>
+    <div className="navbar bg-neutral text-neutral-content">
+      <button className="btn btn-ghost text-xl">FOSSTrak</button>
     </div>
   );
 };
