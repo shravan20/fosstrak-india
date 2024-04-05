1c1f310a23158e4b4ac505a9de23ce6b8298823f
shravan20 <mrshravankumarb@gmail.com>
2024-04-05 20:10:37 +0530

    feat: set navbar component with list event option

--- src/components/atoms/Navbar.jsx
+++ src/components/atoms/Navbar.jsx
@@ -2,9 +2,32 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-neutral text-neutral-content">
-      <button className="btn btn-ghost text-xl">FOSSTrak</button>
-    </div>
+    <>
+      {/* <div className="navbar bg-neutral text-neutral-content">
+        <button className="btn btn-ghost text-xl">FOSSTrak</button>
+
+        <div className="navbar-end">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>List your event</a>
+            </li>
+          </ul>
+        </div>
+      </div> */}
+
+      <div className="navbar bg-neutral">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">FOSSTrak</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li className="primary">
+              <a>List your event!</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
 
