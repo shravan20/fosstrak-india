ac7f55e09d376623908dd40fa96a186ab4e1a073
shravan20 <mrshravankumarb@gmail.com>
2024-04-05 20:13:19 +0530

    refactor: Move codes into structure

--- src/components/atoms/Navbar.jsx
+++ src/components/atoms/Navbar.jsx
@@ -3,18 +3,6 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      {/* <div className="navbar bg-neutral text-neutral-content">
-        <button className="btn btn-ghost text-xl">FOSSTrak</button>
-
-        <div className="navbar-end">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>List your event</a>
-            </li>
-          </ul>
-        </div>
-      </div> */}
-
       <div className="navbar bg-neutral">
         <div className="flex-1">
           <a className="btn btn-ghost text-xl">FOSSTrak</a>
