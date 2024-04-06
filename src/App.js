ba7da863e975bb641fa827cabfacfb96272cbc10
shravan20 <mrshravankumarb@gmail.com>
2024-04-06 12:03:09 +0530

    feat: event page design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Body from './components/templatez/HomePage';
 import Navbar from './components/atoms/Navbar';
+import EventDetail from './components/pages/EventDetail';
 const App = () => {
     return (
         <>
             <Navbar />
             <Body />
+            {/* <EventDetail /> */}
         </>
     );
 };
