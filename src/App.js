955f1590691710336bf2c1fa1197facfd56b7591
shravan20 <mrshravankumarb@gmail.com>
2024-04-13 02:09:33 +0530

    feat: add poster design for events

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './components/atoms/Navbar';
 import Modal from './components/atoms/Modal';
 import CustomisedNavbar from './components/atoms/CustomisedNavbar';
 import EventDetail from './components/pages/EventDetail';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NotFound from './components/pages/NotFound';
 
 const App = () => {
@@ -12,13 +12,13 @@ const App = () => {
         <>
             {/* TODO: Explore CustomizedNavbar ; Make it default or remove later on */}
             <Navbar />
-            <BrowserRouter>
+            <Router>
                 <Routes>
                     <Route path="/" element={<Body />} />
                     <Route path="/home" element={<Body />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
-            </BrowserRouter>
+            </Router>
         </>
     );
 };
